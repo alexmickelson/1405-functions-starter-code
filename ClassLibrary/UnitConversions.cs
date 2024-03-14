dfe3ba74a8fe1207bb00a44c9d653ad2f1a4f1e8
Alex Mickelson <alexmickelson96@gmail.com>
2024-03-13 22:00:25 -0600

    starting code

---
+++ ClassLibrary/UnitConversions.cs
@@ -0,0 +1,28 @@
+﻿
+public static class UnitConversions
+{
+
+  public static decimal InchesToCentimeters(decimal inches)
+  {
+    return 0m;
+  }
+
+  public static double YardsToMeters(double yards)
+  {
+    return 0;
+  }
+
+  public static double MilesToKilometers(decimal miles)
+  {
+    return 0;
+  }
+
+  public static bool IsApproximate(double firstNumber, decimal secondNumber, int precisionAfterDecimalPoint)
+  {
+    // checks for approximate equality between a double and a decimal
+    // this adjusts for midor differences between floating point precision errors
+    // 0.00001 and 0.001 are approximately equal with 2 points of precision after the decimal point
+
+    return false;
+  }
+}
--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -2,9 +2,215 @@ namespace TestProject;
 
 public class UnitTest1
 {
+
+
     [Fact]
-    public void Test1()
+    public void InchesToCentimeters_WorksWithOne()
     {
-        Assert.Equal(1, Program.GetNumber());
+        // Arrange
+        decimal inches = 1m;
+
+        // Act
+        decimal actual = UnitConversions.InchesToCentimeters(inches);
+        decimal expected = 2.54m;
+
+        // Assert
+        Assert.Equal(expected, actual);
     }
+
+    // [Fact]
+    // public void InchesToCentimeters_WorksWithPositiveNumbers()
+    // {
+    //     // Arrange
+    //     decimal inches = 3.5m;
+
+    //     // Act
+    //     decimal actual = UnitConversions.InchesToCentimeters(inches);
+    //     decimal expected = 8.890m;
+
+    //     // Assert
+    //     Assert.Equal(expected, actual);
+    // }
+
+    // [Fact]
+    // public void InchesToCentimeters_WorksWithNegativeNumbers()
+    // {
+    //     // Arrange
+    //     decimal inches = -8.2m;
+
+    //     // Act
+    //     decimal actual = UnitConversions.InchesToCentimeters(inches);
+    //     decimal expected = -20.828m;
+
+    //     // Assert
+    //     Assert.Equal(expected, actual);
+    // }
+
+    // [Fact]
+    // public void InchesToCentimeters_WorksWithZero()
+    // {
+    //     // Arrange
+    //     decimal inches = 0m;
+
+    //     // Act
+    //     decimal actual = UnitConversions.InchesToCentimeters(inches);
+    //     decimal expected = 0.0m;
+
+    //     // Assert
+    //     Assert.Equal(expected, actual);
+    // }
+
+    // [Fact]
+    // public void YardsToMeters_WorksWithOne()
+    // {
+    //     // Arrange
+    //     double yards = 1;
+
+    //     // Act
+    //     double actual = UnitConversions.YardsToMeters(yards);
+    //     double expected = 0.9144;
+
+    //     // Assert
+    //     Assert.Equal(expected, actual);
+    // }
+
+    // [Fact]
+    // public void YardsToMeters_WorksWithImpreciseOne()
+    // {
+    //     // Arrange
+    //     double yards = 1;
+
+    //     // Act
+    //     double actual = UnitConversions.YardsToMeters(yards);
+    //     double expected = 0.91439999999999999;
+
+    //     // Assert
+    //     Assert.Equal(expected, actual);
+    // }
+
+    // [Fact]
+    // public void YardsToMeters_WorksWithNegative()
+    // {
+    //     // Arrange
+    //     double yards = -5.453893;
+
+    //     // Act
+    //     double actual = UnitConversions.YardsToMeters(yards);
+    //     double expected = -4.9870397592;
+
+    //     // Assert
+    //     Assert.Equal(expected, actual);
+    // }
+
+    // [Fact]
+    // public void YardsToMeters_WorksWithZero()
+    // {
+    //     // Arrange
+    //     double yards = 0;
+
+    //     // Act
+    //     double actual = UnitConversions.YardsToMeters(yards);
+    //     double expected = 0.0;
+
+    //     // Assert
+    //     Assert.Equal(expected, actual);
+    // }
+
+    // [Fact]
+    // public void MilesToKilometers_WorksWithPositive()
+    // {
+    //     // Arrange
+    //     decimal miles = 4235.53423m;
+
+    //     // Act
+    //     double actual = UnitConversions.MilesToKilometers(miles);
+    //     double expected = 6816.4315998451202;
+
+    //     // Assert
+    //     Assert.Equal(expected, actual);
+    // }
+
+    // [Fact]
+    // public void MilesToKilometers_WorksWithNegative()
+    // {
+    //     // Arrange
+    //     decimal miles = 4235.53423m;
+
+    //     // Act
+    //     double actual = UnitConversions.MilesToKilometers(miles);
+    //     double expected = 6816.4315998451202;
+
+    //     // Assert
+    //     Assert.Equal(expected, actual);
+    // }
+
+    // [Fact]
+    // public void MilesToKilometers_WorksWithZero()
+    // {
+    //     // Arrange
+    //     decimal miles = 0m;
+
+    //     // Act
+    //     double actual = UnitConversions.MilesToKilometers(miles);
+    //     double expected = 0.0;
+
+    //     // Assert
+    //     Assert.Equal(expected, actual);
+    // }
+    
+    // [Fact]
+    // public void IsApproximate_CanCheckNearEquality()
+    // {
+    //     // Arrange
+    //     double first = 0.001;
+    //     decimal second = 0.002m;
+
+    //     // Act
+    //     bool actual = UnitConversions.IsApproximate(first, second, 2);
+
+    //     // Assert
+    //     Assert.True(actual);
+    // }
+
+    // [Fact]
+    // public void IsApproximate_CanDetectWhenNotApproximate()
+    // {
+    //     // Arrange
+    //     double first = 0.001;
+    //     decimal second = 0.06m;
+
+    //     // Act
+    //     bool actual = UnitConversions.IsApproximate(first, second, 2);
+
+    //     // Assert
+    //     Assert.False(actual);
+    // }
+
+    // [Fact]
+    // public void IsApproximate_WorksWithNumbersLargerThanOne_1()
+    // {
+    //     // Arrange
+    //     double first = 1.001;
+    //     decimal second = 1.007m;
+
+    //     // Act
+    //     bool actual = UnitConversions.IsApproximate(first, second, 2);
+
+    //     // Assert
+    //     Assert.True(actual);
+    // }
+
+    // [Fact]
+    // public void IsApproximate_WorksWithNumbersLargerThanOne_2()
+    // {
+    //     // Arrange
+    //     double first = 1.009;
+    //     decimal second = 1.01m;
+
+    //     // Act
+    //     bool actual = UnitConversions.IsApproximate(first, second, 2);
+
+    //     // Assert
+    //     Assert.False(actual);
+    // }
 }
